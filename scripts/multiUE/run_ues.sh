573f744ed1078f0473ea53c7cce695de0e8b40af
Jon Larrea <jon.larrea@ed.ac.uk>
2022-09-14 13:23:21 +0200

    Multi-UE working

--- scripts/multiUE/run_ues.sh
+++ scripts/multiUE/run_ues.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "$#" -ne 2 ]; then
-    echo "USE: sudo ./run_ues.sh <Number of UEs> <Traffic Commands (quoted)>"
+    echo "USE: ./run_ues.sh <Number of UEs> <Traffic Commands (quoted)>"
     echo "[Example] Run two UEs that ping the SPGW"
-    echo '    sudo ./run_ues.sh 2 "ping -I oaitun_ue1 172.16.0.1"'
+    echo '    ./run_ues.sh 2 "ping -I oaitun_ue1 172.16.0.1"'
     exit 1
 fi
 
